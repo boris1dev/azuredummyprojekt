1be8508f90570e426d96faff739417ce31116014
borisnicolai <mail@boris-nicolai.de>
2023-07-21 20:48:43 +0200

    staging änderungen

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_boris_erste_web_app.Pages
+{
+	public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
